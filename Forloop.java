f5e7d17df13355e2a9ba1d33edc919aa5e071928
Katuruyashwanthsai <137894914+Katuruyashwanthsai@users.noreply.github.com>
2025-10-03 18:02:38 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,10 @@
+//print numbers from 1 to 10 using forloop .
+package com.Loops;
+
+public class Forloop {
+ public static void main(String[]args) {
+for(int i=0;i<=10;i++) {
+	System.out.print(i +" ");
+	}
+}
+}
