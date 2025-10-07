4eb1ff587d48f6d2fd831876a7ec293df323048c
Katuruyashwanthsai <137894914+Katuruyashwanthsai@users.noreply.github.com>
2025-10-07 19:06:27 +0530

    Add files via upload

---
+++ days.java
@@ -0,0 +1,36 @@
+package conditions;
+import java.util.Scanner;
+public class days {
+public static void main (String[]args ){
+		Scanner a= new Scanner(System.in);
+		int d = a.nextInt();
+switch (d) {
+case 1:
+    System.out.println("monday");
+    break;
+case 2:
+    System.out.println("tuesday");
+    break;
+case 3:
+    System.out.println("wensday");
+    break;
+case 4:
+    System.out.println("thursday");
+    break;
+case 5:
+    System.out.println("friday");
+    break;
+case 6:
+    System.out.println("saturday");
+    break;
+case 7:
+    System.out.println("sunday");
+    break;
+case 8:
+    System.out.println("invalid");
+    break;
+
+      
+}
+}
+}
